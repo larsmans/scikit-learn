b2c80d7e83b17827e0be370ae8502c933654cc4c
Tadej Janež <tadej.janez@tadej.hicsalta.si>
2013-03-03 14:25:43 +0100

    ENH auc_score should throw an error when y_true isn't binary

--- sklearn/metrics/tests/test_metrics.py
+++ sklearn/metrics/tests/test_metrics.py
@@ -9,6 +9,7 @@ from sklearn import svm
 
 from sklearn.utils.testing import (assert_true,
                                    assert_raises,
+                                   assert_raise_message,
                                    assert_equal,
                                    assert_almost_equal,
                                    assert_not_equal,
@@ -194,6 +195,27 @@ def test_auc_errors():
 
     # Too few x values
     assert_raises(ValueError, auc, [0.0], [0.1])
+
+
+def test_auc_score_non_binary_class():
+    """Test that auc_score function returns an error when trying to compute AUC
+    for non-binary class values.
+    """
+    y_pred = np.random.rand(10)
+    # y_true contains only one class value
+    y_true = np.zeros(10, dtype="int")
+    assert_raise_message(ValueError, "AUC is defined for binary "
+                         "classification only", auc_score, y_true, y_pred)
+    y_true = np.ones(10, dtype="int")
+    assert_raise_message(ValueError, "AUC is defined for binary "
+                         "classification only", auc_score, y_true, y_pred)
+    y_true = -np.ones(10, dtype="int")
+    assert_raise_message(ValueError, "AUC is defined for binary "
+                         "classification only", auc_score, y_true, y_pred)
+    # y_true contains three different class values
+    y_true = np.random.randint(0, 3, size=10)
+    assert_raise_message(ValueError, "AUC is defined for binary "
+                         "classification only", auc_score, y_true, y_pred)
 
 
 def test_precision_recall_f1_score_binary():
